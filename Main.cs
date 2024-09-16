46b29e39af569bbdb77b82a605c3d4590756ce4b
Xtarii <56205541+Xtarii@users.noreply.github.com>
2024-09-16 09:29:06 +0200

    Create Main.cs
    Hur man skriver Hello World i C#
    ------------------------------------
    
    För jag är trött på att programmerare inte
    vet att man inte behöver skriva ut ett
    - Namespace
    - Class
    - Static void Main

---
+++ Main.cs
@@ -0,0 +1,12 @@
+/// Hello World Program in C#
+namespace HelloWorld {
+  public class Program {
+    public static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
+
+
+/// Method 2
+Console.WriteLine("Hello World!");
